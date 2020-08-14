48a74d9336352b1591d9c640fc25c02c73ab3dd7
lairase <ovidio.lairana@some.com>
2020-08-14 16:10:59 +0200

    misc

--- src/app/common/mock/audit-log.mock.ts
+++ src/app/common/mock/audit-log.mock.ts
@@ -4,8 +4,17 @@ export function getAuditLogMocks(size: number = 32): AuditLogModel[] {
   const auditLogSet: AuditLogModel[] = [];
 
   for (let i = 1; i <= size; i++) {
-    auditLogSet.push(new AuditLogModel('' + i, '08/08/2020 ' + i, 'action_' + i, 'username_' + i, 'comment_' + i, 'link_' + i));
+    auditLogSet.push(new AuditLogModel('' + i,
+      '08/08/2020 ' + i,
+      'action_' + i,
+      'username_' + i,
+      getComment('comment_' + i),
+      'link_' + i));
   }
 
   return auditLogSet;
+}
+
+function getComment(initialComment): string {
+  return initialComment + '     very big comment here that could take out tables cells';
 }
