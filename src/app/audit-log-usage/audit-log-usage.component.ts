12d56c7459a32495190bc624a972a255fdcc5333
lairase <ovidio.lairana@some.com>
2020-08-17 15:07:12 +0200

    misc

--- src/app/audit-log-usage/audit-log-usage.component.ts
+++ src/app/audit-log-usage/audit-log-usage.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-audit-log-usage',
-  templateUrl: './audit-log-usage.component.html',
-  styleUrls: ['./audit-log-usage.component.scss']
+  templateUrl: './audit-log-usage.component.html'
 })
 export class AuditLogUsageComponent {}
